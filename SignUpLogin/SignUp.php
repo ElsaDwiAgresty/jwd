3d44caaeaa8fc5ade6c72ba91c1e40ccd155999c
ElsaDwiAgresty <165319077+ElsaDwiAgresty@users.noreply.github.com>
2024-07-25 05:12:49 +0700

    Commit perubahan file SignUp
    penambahan bagian backend untuk signup (belum sempurna)

--- SignUpLogin/SignUp.php
+++ SignUpLogin/SignUp.php
@@ -1,16 +1,20 @@
 <?php
+include 'koneksi.php';
 
-if (isset($SignUp['$_POST'])) {
-    $nama = 'nama';
-    $email = 'email';
-    $password = 'password';
+if (isset($_POST['signup'])) {
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "INSERT INTO data (nama, email, password) VALUES ('$nama', '$email', '$password')";
+
+    if (mysqli_query($connection, $query)) {
+        echo "Sign Up berhasil silahkan login";
+        echo "<a href='Login.html'>Login</a>";
+    } else {
+        echo "Sign Up mengalami masalah" . $query . mysqli_error($connection);
+    }
 }
 
-$query = "INSERT INTO data (nama, email, password) VALUES ('$nama', '$email', '$password')";
 
-if ($connection->$query === TRUE) {
-    echo "Sign Up berhasil silahkan login";
-    echo "<a href='Login.html'>Login</a>";
-} else {
-    echo "Sign Up mengalami masalah" . $query . mysqli_error($connection);
-}
+?>
