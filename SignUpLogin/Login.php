21947c12541403b9b36ef0f5c7a815bca46a3bd6
ElsaDwiAgresty <165319077+ElsaDwiAgresty@users.noreply.github.com>
2024-07-25 23:32:02 +0700

    Update penambahan backend Login
    backend login + halaman index (masih error)

--- SignUpLogin/Login.php
+++ SignUpLogin/Login.php
@@ -1,2 +1,21 @@
 <?php
 
+include 'koneksi.php';
+session_start();
+
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT*FROM data WHERE email = '$email' AND password = '$password'";
+
+    if (mysqli_num_rows(mysqli_query($connection, $query)) == 1) {
+        $_SESSION['user'] = $email;
+        header("Location: index.php");
+        exit();
+    } else {
+        $error_message = "Invalid email or password!";
+    }
+}
+
+?>
---
+++ SignUpLogin/index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit();
+}
+
+echo "<h1>Welcome, " . $_SESSION['user'] . "!</h1>";
+echo "<a href='logout.php'>Logout</a>";
+
+?>
