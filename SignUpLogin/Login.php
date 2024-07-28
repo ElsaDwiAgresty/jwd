74710881f4258cf5682f7a7afbe6f6fdcfd36e5b
ElsaDwiAgresty <165319077+ElsaDwiAgresty@users.noreply.github.com>
2024-07-28 11:00:15 +0700

    Commit perubahan halaman login dan index
    penambahan konfirmasi logout dan halaman index

--- SignUpLogin/Login.php
+++ SignUpLogin/Login.php
@@ -14,7 +14,28 @@ if (isset($_POST['login'])) {
         header("Location: index.php");
         exit();
     } else {
-        $error_message = "Invalid email or password!";
+        echo'
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link
+            href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+            rel="stylesheet"
+            />
+            <title>Try Again</title>
+        </head>
+        <body>
+            <div class="flex justify-center">
+                <div class="text-center box-content h-52 w-64 p-4 border-2 border-red-700 mt-36 flex flex-col items-center" bg-slate-600>
+                    <p class="text-xl font-bold mt-10 text-red-600">Invalid email or password! Please Try Again</p><br>
+                    <button class="bg-blue-600 text-white rounded-lg h-8 w-24 mt-6 font-serif"><a href= "Login.html">Try Again</a>
+                </div><br><br>
+            </div>
+        
+            </body>
+        </html>';
     }
 }
 
--- SignUpLogin/index.php
+++ SignUpLogin/index.php
@@ -6,7 +6,31 @@ if (!isset($_SESSION['user'])) {
     exit();
 }
 
-echo "<h1>Welcome, " . $_SESSION['user'] . "!</h1>";
-echo "<a href='logout.php'>Logout</a>";
-
+echo "<h1 class='text-xl font-bold px-10 font-serif mt-5'>Welcome, " . $_SESSION['user'] . "!</h1>";
 ?>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+            rel="stylesheet"
+            />
+            <title>logout</title>
+            <script>
+                function confirmLogout(){
+                    var confirmLogout = confirm("Are you sure want to logout?")
+                    if(confirmLogout){
+                        window.location.href ="Login.html"
+                    }
+                }
+            </script>
+        </head>
+        <body>
+            <div class="px-10">
+                <button onclick="confirmLogout()" class="rounded-lg bg-red-600 text-white h-8 w-20"><a href="logout.php">Logout</a></button>
+            </div>
+        </body>
+    </html>
+
+
